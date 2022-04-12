2bb3776b33fa0f40a3e8d93fe953ab1c8150e28d
BatyrArtem <artembatyr88@gmail.com>
2022-04-12 21:56:19 +0300

    Updates

---
+++ data.js
@@ -0,0 +1,40 @@
+const characterData = {
+    zelensky: {
+        name: "Zelensky",
+        avatar: "images/zelensky.jpg",
+        health: 100,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orks: {
+        name: "Russian Orks",
+        avatar: "images/yebok.jpg",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    gondon: {
+        name: "Gandon",
+        avatar: "images/gondon.jpg",
+        health: 25,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    mordor: {
+        name: "Mordor",
+        avatar: "images/kremlin.jpg",
+        health: 20,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    putin: {
+        name: "Putin",
+        avatar: "images/putin.jpg",
+        health: 20,
+        diceCount: 2,
+        currentDiceScore: []
+    }
+}
+
+
+export default characterData;
